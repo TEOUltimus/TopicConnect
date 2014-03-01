99f120f512e52b8c656639c7373899bd0fa33720
Eric <esummers@andrew.cmu.edu>
2014-03-01 11:06:19 -0500

    new interfaces

--- Plaftform.java
+++ Plaftform.java
@@ -4,7 +4,7 @@
 public class Plaftform {
 
 	/**
-	 * Allows users to find other users talking about similar topics
+ * Allows users to find other users talking about similar topics
 	 * @param args
 	 */
 	public static void main(String[] args) {
