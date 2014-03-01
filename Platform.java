c4e32fdd4778ba92d20140a1770ba2d72da6bff2
Eric <esummers@andrew.cmu.edu>
2014-03-01 11:08:42 -0500

    fixed spelling

---
+++ Platform.java
@@ -0,0 +1,18 @@
+/**
+ * @author TEO Ultimus
+ */
+public class Platform {
+	private static Platform singleton;
+	
+	private Platform(){
+	}
+	
+	/**
+	 * Allows users to find other users talking about similar topics
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		System.out.println("Hello Users!");
+	}
+
+}
