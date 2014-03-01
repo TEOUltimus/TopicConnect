d2bdc2632e13ebacc7eb0a4d1f55aa06f087cef6
Eric <esummers@andrew.cmu.edu>
2014-03-01 10:51:20 -0500

    Added Initial Platform

---
+++ Plaftform.java
@@ -0,0 +1,14 @@
+/**
+ * @author TEO Ultimus
+ */
+public class Plaftform {
+
+	/**
+	 * Allows users to find other users talking about similar topics
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		System.out.println("Hello Users!");
+	}
+
+}
