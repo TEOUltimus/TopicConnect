62cc608c0851dde41f269f825fb6057e09995b0c
Brandon Sherman <mechcozmo@gmail.com>
2014-03-01 13:26:34 -0500

    Added DB code

--- Platform.java
+++ Platform.java
@@ -1,15 +1,24 @@
 import java.util.Collection;
 import java.util.HashSet;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.sql.ResultSet;
+
+
+// Not importing com.mysql.jdbc, documentation says this leads to sadness
 
 /**
  * @author TEO Ultimus
  */
 public class Platform {
 	private static Platform singleton = null;
+	public static Connection SQLconn = null;
 	Collection<User> users;
 	
 	private Platform(){
-		users = new HashSet<>();
+//		users = new HashSet<>();
 	}
 	
 	public static Platform getPlatform() {
@@ -33,5 +42,17 @@ public class Platform {
 	 */
 	public static void main(String[] args) {
 		System.out.println("Hello Users!");
+		String sqlconnstring = "jdbc:mysql://mylanhack.coktmzoprhqd.us-west-2.rds.amazonaws.com/mylanhack?" +
+                                   "user=mylanroot&password=mylantoor";
+		
+		// Establish Database Connection
+		try{
+		    Class.forName("com.mysql.jdbc.Driver").newInstance();
+		    SQLconn = DriverManager.getConnection(sqlconnstring);
+		}
+		catch(Exception ex){
+		    System.out.println("Database connection went bad -- " + ex);
+		}
+		
 	}
 }
